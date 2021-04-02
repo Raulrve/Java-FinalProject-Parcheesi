e97bf56533f64ef029c026900442c5c33d0aaa9d
DiegoStm3701 <diego3701@gmail.com>
2021-04-02 19:01:52 -0400

    Add players and start button

--- src/InitialMenu.java
+++ src/InitialMenu.java
@@ -1,8 +1,11 @@
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.Image;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -13,22 +16,88 @@ public class InitialMenu extends JFrame{
 	
 	public void choosePlayers() {
 		
-		JPanel playerNumber = new JPanel();
 		
-		playerNumber.setLocation(400,500);
-		playerNumber.setSize(600,200);
-		playerNumber.setBackground(backgroundColor);
+		
+		JPanel playerQuestion = new JPanel();
+		
+		playerQuestion.setLocation(400,300);
+		playerQuestion.setSize(600,30);
+		playerQuestion.setBackground(backgroundColor);
 		JLabel howMany = new JLabel("Please choose a number of players");
 		howMany.setFont(new Font("Calibri",Font.PLAIN,25));
 		
-		playerNumber.add(howMany);
-		background.add(playerNumber);
+		playerQuestion.add(howMany);
+		background.add(playerQuestion);
+		
+		
+		JPanel buttonPanel = new JPanel();
+		buttonPanel.setLayout(null);
+		buttonPanel.setLocation(550,400);
+		buttonPanel.setSize(300,50);
+		buttonPanel.setBackground(backgroundColor);
+		
+		
+		JLabel numberLabel = new JLabel(""+playerNumber);
+		numberLabel.setFont(new Font("Calibri",Font.PLAIN,25));
+		numberLabel.setSize(30,50);
+		numberLabel.setLocation(140,0);
+		buttonPanel.add(numberLabel);
+		background.add(buttonPanel);
+		
+		JButton removePlayer = new JButton("-");
+		removePlayer.setFont(new Font("Calibri",Font.BOLD,25));
+		removePlayer.setLocation(70,0);
+		removePlayer.setSize(50,50);
+		removePlayer.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				playerNumber--;
+				if(playerNumber < 2) {
+					playerNumber = 2;
+				}
+				numberLabel.setText(""+playerNumber);
+			}
+		});
+		buttonPanel.add(removePlayer);
+		
+		JButton addPlayer = new JButton("+");
+		addPlayer.setFont(new Font("Calibri",Font.BOLD,25));
+		addPlayer.setLocation(170,0);
+		addPlayer.setSize(50,50);
+		addPlayer.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				playerNumber++;
+				if(playerNumber > 4) {
+					playerNumber = 4;
+				}
+				numberLabel.setText(""+playerNumber);
+				
+			}
+		});
+		buttonPanel.add(addPlayer);
+		
+		
+		
+		JPanel startPanel = new JPanel();
+		startPanel.setLayout(null);
+		startPanel.setLocation(550,500);
+		startPanel.setSize(300,200);
+		startPanel.setBackground(Color.white);
+		background.add(startPanel);
+		
+		JButton startButton = new JButton("START GAME!");
+		startButton.setFont(new Font("Calibri",Font.PLAIN,35));
+		startButton.setSize(300,200);
+		startButton.setLocation(0,0);
+		
+		startPanel.add(startButton);
 		
 	}
 	
 	public void createBackground() {
-		
-		
 		background.setLayout(null);
 		background.setBackground(backgroundColor);
 		
@@ -39,9 +108,15 @@ public class InitialMenu extends JFrame{
 		
 	}
 	
+	public void creators() {
+		JPanel creatorPanel = new JPanel();
+		
+		
+	}
+	
 	private Color backgroundColor = new Color(250,225,192);
 	private JPanel background = new JPanel();
-	private int playerNumber;
+	private int playerNumber = 2;
 	
 	
 }
